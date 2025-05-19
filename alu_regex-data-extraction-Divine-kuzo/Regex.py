9846671838fb2116b135fbd035534a628ca0301d
Divine-kuzo <d.kuzo@alustudent.com>
2025-05-19 18:38:16 +0200

    extract_urls

--- alu_regex-data-extraction-Divine-kuzo/Regex.py
+++ alu_regex-data-extraction-Divine-kuzo/Regex.py
@@ -2,4 +2,8 @@ import re
 
 def extract_emails(text):
     pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+    return re.findall(pattern, text)def extract_urls(text):
+    pattern = r'https?://(?:www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z]{2,6}\b(?:[-a-zA-Z0-9@:%\+.~#?&/=]*)'
     return re.findall(pattern, text)
+
+
