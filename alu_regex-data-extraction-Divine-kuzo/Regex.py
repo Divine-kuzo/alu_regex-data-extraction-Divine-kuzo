c34ab93eb32c01f173a274e34da6387777fa06df
Divine-kuzo <d.kuzo@alustudent.com>
2025-05-19 18:51:03 +0200

    credit

--- alu_regex-data-extraction-Divine-kuzo/Regex.py
+++ alu_regex-data-extraction-Divine-kuzo/Regex.py
@@ -7,11 +7,19 @@ def extract_emails(text):
     return re.findall(pattern, text)
 
 def extract_urls(text):
-    pattern = r'https?://(?:www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z]{2,6}\b(?:[-a-zA-Z0-9@:%\+.~#?&/=]*)'
+    patterdef extract_credit_card_numbers(text):
+    pattern = r'\b(?:\d{4}[-\s]?){3}\d{4}\b'
+    return re.findall(pattern, text)
+def extract_time(text):
+    pattern = r'\b(?:[01]?\d|2[0-3]):[0-5]\d(?:\s?[APap][Mm])?\b'
+    return re.findall(pattern, text)
+n = r'https?://(?:www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z]{2,6}\b(?:[-a-zA-Z0-9@:%\+.~#?&/=]*)'
     return re.findall(pattern, text)
 
 def extract_phone_numbers(text):
     pattern = r'\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}'
     return re.findall(pattern, text)
+
+
 
 
