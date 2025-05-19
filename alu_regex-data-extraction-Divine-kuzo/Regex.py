59307f840f51a4e3e026295a2f84dc4f59e0bfc8
Divine-kuzo <d.kuzo@alustudent.com>
2025-05-19 20:05:32 +0200

    correction

--- alu_regex-data-extraction-Divine-kuzo/Regex.py
+++ alu_regex-data-extraction-Divine-kuzo/Regex.py
@@ -2,18 +2,10 @@ import re
 
 def extract_emails(text):
     pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
-    return re.findall(pattern, text)def extract_urls(text):
-    pattern = r'https?://(?:www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z]{2,6}\b(?:[-a-zA-Z0-9@:%\+.~#?&/=]*)'
     return re.findall(pattern, text)
 
 def extract_urls(text):
-    patterdef extract_credit_card_numbers(text):
-    pattern = r'\b(?:\d{4}[-\s]?){3}\d{4}\b'
-    return re.findall(pattern, text)
-def extract_time(text):
-    pattern = r'\b(?:[01]?\d|2[0-3]):[0-5]\d(?:\s?[APap][Mm])?\b'
-    return re.findall(pattern, text)
-n = r'https?://(?:www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z]{2,6}\b(?:[-a-zA-Z0-9@:%\+.~#?&/=]*)'
+    pattern = r'https?://(?:www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z]{2,6}\b(?:[-a-zA-Z0-9@:%\+.~#?&/=]*)'
     return re.findall(pattern, text)
 
 def extract_phone_numbers(text):
@@ -23,10 +15,10 @@ def extract_phone_numbers(text):
 def extract_credit_card_numbers(text):
     pattern = r'\b(?:\d{4}[-\s]?){3}\d{4}\b'
     return re.findall(pattern, text)
+
 def extract_time(text):
     pattern = r'\b(?:[01]?\d|2[0-3]):[0-5]\d(?:\s?[APap][Mm])?\b'
     return re.findall(pattern, text)
-
 
 def extract_html_tags(text):
     pattern = r'<[^>]+>'
